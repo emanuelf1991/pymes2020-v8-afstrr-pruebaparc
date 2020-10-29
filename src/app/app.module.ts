e0f7706e06307a2da5407de7019802e463cf9c0c
emanuelf1991 <emanuel.fernandez1991@gmail.com>
2020-10-29 16:21:56 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { InicioComponent } from "./components/inicio/inicio.component";
 import { ArticulosComponent } from "./components/articulos/articulos.component";
 import { ArticulosFamiliasComponent } from "./components/articulos-familias/articulos-familias.component";
 import { ModalDialogComponent } from "./components/modal-dialog/modal-dialog.component";
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { ModalDialogComponent } from "./components/modal-dialog/modal-dialog.com
     ArticulosComponent,
     ArticulosFamiliasComponent,
     ModalDialogComponent,
+    ServiciosComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,6 +38,7 @@ import { ModalDialogComponent } from "./components/modal-dialog/modal-dialog.com
     RouterModule.forRoot([
       { path: '', redirectTo: '/inicio', pathMatch: 'full' },
       { path: 'inicio', component: InicioComponent },
+      { path: 'servicios', component: ServiciosComponent },
       { path: 'articulos', component: ArticulosComponent },
       { path: 'articulosfamilias', component: ArticulosFamiliasComponent }
     ]),
