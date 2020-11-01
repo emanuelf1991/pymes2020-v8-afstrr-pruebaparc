29df51a0964750a28ab7a8a4206a2a8294a413b9
emanuelf1991 <emanuel.fernandez1991@gmail.com>
2020-11-01 18:55:02 -0300

    2 files modified

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -16,7 +16,7 @@ export class ClientesService {
   constructor(private httpClient: HttpClient) {
     // la barra al final del resourse url es importante para los metodos que concatenan el id del recurso (GetById, Put)
     //this.resourceUrl = "https://pavii.ddns.net/api/articulos/";
-    this.resourceUrl = "https://bitgocba.duckdns.org/api/articulos/";
+    this.resourceUrl = "https://demo3151356.mockable.io/clientes/";
   }
 
   get() {
