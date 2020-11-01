54e5857817b76a25ac4160dbf01eb3b70480efd8
emanuelf1991 <emanuel.fernandez1991@gmail.com>
2020-11-01 19:02:48 -0300

    2 files modified

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -67,22 +67,22 @@ export class ClientesComponent implements OnInit {
         "",
         [Validators.required, Validators.minLength(4), Validators.maxLength(55)]
       ],
-      Precio: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
-      Stock: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
-      CodigoDeBarra: [
-        "",
-        [Validators.required, Validators.pattern("[0-9]{13}")]
-      ],
+      NumeroDocumento: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
+      // Stock: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
+      // CodigoDeBarra: [
+      //   "",
+      //   [Validators.required, Validators.pattern("[0-9]{13}")]
+      // ],
       //IdArticuloFamilia: ["", [Validators.required]],
-      FechaAlta: [
-        "",
-        [
-          Validators.required,
-          Validators.pattern(
-            "(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}"
-          )
-        ]
-      ],
+      // FechaAlta: [
+      //   "",
+      //   [
+      //     Validators.required,
+      //     Validators.pattern(
+      //       "(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}"
+      //     )
+      //   ]
+      // ],
       Activo: [true]
     });
 
@@ -95,13 +95,13 @@ export class ClientesComponent implements OnInit {
   //   });
   // }
 
-  Agregar() {
-    this.AccionABMC = "A";
-    this.FormReg.reset();
-    this.submitted = false;
-    //this.FormReg.markAsPristine();
-    this.FormReg.markAsUntouched();
-  }
+  // Agregar() {
+  //   this.AccionABMC = "A";
+  //   this.FormReg.reset();
+  //   this.submitted = false;
+  //   //this.FormReg.markAsPristine();
+  //   this.FormReg.markAsUntouched();
+  // }
 
   // Buscar segun los filtros, establecidos en FormReg
   Buscar() {
